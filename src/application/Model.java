8e23e022038f4b9d5baa97e199711329447549e9
JAVED SHAIK <jshaik@terpmail.umd.edu>
2017-01-02 19:57:10 -0500

    Add files via upload

---
+++ src/application/Model.java
@@ -0,0 +1,25 @@
+package application;
+
+public class Model {
+	public float Calculate(long num1, long num2, String operator) {
+
+		switch (operator) {
+		case "+":
+			return num1 + num2;
+		case "-":
+			return num1 - num2;
+		case "*":
+			return num1 * num2;
+		case "AC":
+				return 0;
+		
+		case "/":
+			if (num2 == 0) 
+				return 0;
+				return num1 / num2;
+		default:
+			return 0;
+		}
+
+	}
+}
